9ade873797f12b0d534c872421935af1172dc311
Peter CHEN <yongdechen@gmail.com>
2015-12-24 10:45:11 +1100

    finish full-responsive table.

--- public/metamorphosis/build/js/app-compiled.js
+++ public/metamorphosis/build/js/app-compiled.js
@@ -71,6 +71,32 @@
 
 })();
 (function () {
+	var dM = angular.module('rtdb.dir');
+
+	dM.directive('tableTitle', [function(){
+		return {
+			scope: {
+				headerdib: '@'
+			}, // {} = isolate, true = child, false/undefined = no change
+			controller: function($scope, $element, $attrs, $transclude) {
+				$scope.tableheader = $scope.headerdib;
+				console.log('what is $scope.headerdib : '+$scope.headerdib);
+			},
+			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+			restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+			// template: '',
+			templateUrl: './partials/components/table-title.html',
+			// replace: true,
+			// transclude: true,
+			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+			link: function($scope, iElm, iAttrs, controller) {
+				
+			}
+		};
+	}]);
+
+})();
+(function () {
 	var cdM = angular.module('rtdb.cust.dir');
 
 	cdM.directive('responsiveTableUnseen', [function(){
@@ -157,32 +183,6 @@
 		};
 	}]);
 
-
-})();
-(function () {
-	var dM = angular.module('rtdb.dir');
-
-	dM.directive('tableTitle', [function(){
-		return {
-			scope: {
-				headerdib: '@'
-			}, // {} = isolate, true = child, false/undefined = no change
-			controller: function($scope, $element, $attrs, $transclude) {
-				$scope.tableheader = $scope.headerdib;
-				console.log('what is $scope.headerdib : '+$scope.headerdib);
-			},
-			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-			restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
-			// template: '',
-			templateUrl: './partials/components/table-title.html',
-			// replace: true,
-			// transclude: true,
-			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-			link: function($scope, iElm, iAttrs, controller) {
-				
-			}
-		};
-	}]);
 
 })();
 // service js Document
